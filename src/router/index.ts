d318e32c12a0b7ff449b7b0514fd13c49c058a2c
Vincent WENDLING <vincent@starling-burgers.com>
2022-12-01 21:39:09 +0100

    :tada: initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
